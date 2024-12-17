e52ff37f2dad8fdb63ff329c72e1cf2e09ad8558
Gokulakrishnan1998 <krishnangokul1998ks@gmail.com>
2024-12-17 15:54:52 +0530

    Added File menu to the window

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import tkinter as tk
 windows = []
 
 def set_menu(win):
+    file_menu = {
+        'New': 'Ctrl + N', 
+        'New Window': 'Ctrl + Shift + N',
+        'Open': 'Ctrl + O',
+        'Save': 'Ctrl + S',
+        'Save As': 'Ctrl + Shift + S',
+        'Page Setup': '',
+        'Print': 'Ctrl + P',
+        'Exit':'',
+    }
     menubar = tk.Menu()
     for i in "File,Edit,Format,View,Help".split(','):
         menu = tk.Menu(menubar)
+        if i == 'File':
+            [ menu.add_command(label=j, accelerator=file_menu.get(j)) if j!='-' else menu.add_separator()
+                for j in "New,New Window,Open,Save,Save As,-,Page Setup,Print,-,Exit".split(',')]
         menubar.add_cascade(label=i, menu=menu)
     win.config(menu=menubar)
 
