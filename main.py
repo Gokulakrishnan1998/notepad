6b540db63d8c6bc29ae7f7023f7ebc39b1a4f416
Gokulakrishnan1998 <krishnangokul1998ks@gmail.com>
2024-12-18 13:15:58 +0530

    Added remaining Menu Items

--- main.py
+++ main.py
@@ -13,12 +13,49 @@ def set_menu(win):
         'Print': 'Ctrl + P',
         'Exit':'',
     }
+    edit_menu = {
+        "Undo":'Ctrl + Z',
+        "Cut":'Ctrl + X',
+        "Copy":'Ctrl + C',
+        "Paste":'Ctrl + V',
+        "Delete":'Del',
+        "SearchWithBing":'Ctrl + E',
+        "Find":'Ctrl + F',
+        "FindNext":'F3',
+        "FindPrevious":'Shift + F3',
+        "Replace":'Ctrl + H',
+        "GoTo":'Ctrl + G',
+        "SelectAll":'Ctrl + A',
+        "TimeDate":'F5',
+    }
+    view_menu = {
+        "ZoomIn":'Ctrl + Plus',
+        "ZoomOut":'Ctrl + Minus',
+        "DefaultZoom":'Ctrl + 0',
+    }
     menubar = tk.Menu()
     for i in "File,Edit,Format,View,Help".split(','):
-        menu = tk.Menu(menubar)
+        menu = tk.Menu(menubar, tearoff=0)
+
         if i == 'File':
             [ menu.add_command(label=j, accelerator=file_menu.get(j)) if j!='-' else menu.add_separator()
                 for j in "New,New Window,Open,Save,Save As,-,Page Setup,Print,-,Exit".split(',')]
+        
+        if i == 'Edit':
+            [ menu.add_command(label=j, accelerator=edit_menu.get(j)) if j!='-' else menu.add_separator()
+                for j in "Undo,-,Cut,Copy,Paste,Delete,-,Search with bing,Find,FindNext,FindPrevious,Replace,GoTo,-,SelectAll,TimeDate".split(',')]
+
+        if i == 'Format':
+            [ menu.add_command(label=j) if j!='-' else menu.add_separator()
+                for j in "WordWrap,Font".split(',')]
+
+        if i == 'View':
+            [ menu.add_command(label=j, accelerator=view_menu.get(j)) if j!='-' else menu.add_separator()
+                for j in "ZoomIn,ZoomOut,DefaultZoom,StatusBar".split(',')]
+
+        if i == 'Help':
+            [ menu.add_command(label=j) if j!='-' else menu.add_separator()
+                for j in "ViewHelp,SendFeedBack,AboutNotePad".split(',')]
         menubar.add_cascade(label=i, menu=menu)
     win.config(menu=menubar)
 
